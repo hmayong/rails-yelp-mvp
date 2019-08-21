eae3219e963195bc5a2bb86623cc925262477f25
mayong <hhmayong@gmail.com>
2019-08-21 16:56:25 +0800

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,11 @@ class ReviewsController < ApplicationController
     # we need `restaurant_id` to associate review with corresponding restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      render "restaurants/show"
+    else
+      render :new
+    end
   end
 
   private
