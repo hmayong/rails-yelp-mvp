c3005fdfeb05de7b1f8587b6fc8e353f751d6010
mayong <hhmayong@gmail.com>
2019-08-21 15:44:05 +0800

    restaurants index, show and add done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,34 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit; end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
